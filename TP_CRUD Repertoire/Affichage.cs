0f7cfb909e84709ac3a3304fc748111c070d7444
Philippe Canduro <philippe.canduro@gmail.com>
2019-09-19 11:51:44 +0200

    refactoring

---
+++ TP_CRUD Repertoire/Affichage.cs
@@ -0,0 +1,149 @@
+﻿using System;
+
+namespace TP_CRUD_Repertoire
+{
+    class Affichage
+    {
+        internal static string AfficherMenu()
+        {
+            Console.WriteLine("\t REPERTOIRE");
+            Console.WriteLine("\t ----------");
+            Console.WriteLine("(1) - Créer une fiche");
+            Console.WriteLine("(2) - Rechercher une fiche");
+            Console.WriteLine("(3) - Mettre a jour une fiche");
+            Console.WriteLine("(4) - Supprimer une fiche");
+            Console.WriteLine("(5) - Afficher toutes les fiches");
+            Console.WriteLine("(6) - Quitter");
+            Console.WriteLine();
+            Console.Write("Choix : ");
+
+            string choixMenu;
+
+            while (true)
+            {
+                choixMenu = Console.ReadLine();
+                if ((choixMenu == "1") || (choixMenu == "2") || (choixMenu == "3") || (choixMenu == "4") || (choixMenu == "5") || (choixMenu == "6")) break;
+                else
+                {
+                    Console.WriteLine("**** Mauvaise saisie ****");
+                    Console.Write("Choix : ");
+                }
+            }
+            Console.Clear();
+            return choixMenu;
+
+        }
+
+        internal static bool SwitchMenu(string choixMenu)
+        {
+            bool continuer = true;
+            switch (choixMenu)
+            {
+                case "1":
+                    DAL.CreerFiche();
+                    PressKey();
+                    break;
+                case "2":
+                    DAL.RechercheFiche();
+                    PressKey();
+                    break;
+                case "3":
+                    DAL.MAJFiche();
+                    PressKey();
+                    break;
+                case "4":
+                    DAL.SupprimerFiche();
+                    PressKey();
+                    break;
+                case "5":
+                    Affichage.AfficherFiches();
+                    PressKey();
+                    break;
+                case "6":
+                    continuer = false;
+                    break;
+                default:
+                    Console.WriteLine("erreur");
+                    break;
+            }
+            return continuer;
+        }
+
+        internal static void AfficherFiches()
+        {
+            Console.WriteLine("\t \tListe de toutes les fiches");
+            Console.WriteLine("\t \t--------------------------");
+            Console.WriteLine($"{"N°",5}|{"Nom",-15}|{"Prenom",-15}|{"Téléphone",15}|{"CP",10}");
+            Console.WriteLine("-----------------------------------------------------------------");
+            for (int i = 0; i < DAL.repertoireActuel.Length; i++)
+            {
+                string fiche = DAL.repertoireActuel[i];
+                int index = i + 1;
+                Console.WriteLine($"{index,5}|{fiche.Split(';')[0],-15}|{fiche.Split(';')[1],-15}|{fiche.Split(';')[2],15}|{fiche.Split(';')[3],10}");
+            }
+
+
+        }
+
+        internal static void AfficherFiches(int position)
+        {
+            string fiche = DAL.repertoireActuel[position];
+            int index = position + 1;
+            Console.WriteLine($"{"N°",5}|{"Nom",-15}|{"Prenom",-15}|{"Téléphone",15}|{"CP",10}");
+            Console.WriteLine("-----------------------------------------------------------------");
+            Console.WriteLine($"{index,5}|{fiche.Split(';')[0],-15}|{fiche.Split(';')[1],-15}|{fiche.Split(';')[2],15}|{fiche.Split(';')[3],10}");
+        }
+
+        internal static void RemplirFiche(ref string newFiche, string message, string separateur)
+        {
+            Console.Write(message);
+            newFiche += Console.ReadLine() + separateur;
+        }
+
+        internal static void PressKey()
+        {
+            Console.WriteLine();
+            Console.WriteLine("Appuyez sur une touche");
+            Console.ReadKey();
+            Console.Clear();
+        }
+
+        internal static void RemplirFicheNum(ref string newFiche, string message, string separateur)
+        {
+            Console.Write(message);
+            int result;
+            while (!int.TryParse(Console.ReadLine(), out result))
+            {
+                Console.WriteLine("Saisie invalide");
+                Console.Write(message);
+            }
+
+            newFiche += result + separateur;
+        }
+
+        internal static void RemplirFicheText(ref string newFiche, string message, string separateur)
+        {
+            string saisie;
+            bool onlychar = false;
+
+            do
+            {
+                Console.Write(message);
+                saisie = Console.ReadLine();
+                onlychar = true;
+                for (int i = 0; i < saisie.Length; i++)
+                    if (!char.IsLetter(saisie[i])) onlychar = false;
+
+                if (onlychar == false) Console.WriteLine("Saisie invalide");
+
+            } while (!onlychar);
+
+            newFiche += saisie + separateur;
+
+
+        }
+
+    }
+
+}
+
---
+++ TP_CRUD Repertoire/DAL.cs
@@ -0,0 +1,123 @@
+﻿using System;
+
+namespace TP_CRUD_Repertoire
+{
+    class DAL
+    {
+
+        internal static string[] repertoireActuel = CreationBase();
+
+        internal static string[] CreationBase()
+        {
+            string[] repertoire = {
+            "Ramon;Robert;0112356497;93160",
+            "Melou;Jeanne;0618976543;77420",
+            "Viale;Christian;0243058976;53250" };
+
+            return repertoire;
+        }
+
+        internal static void CreerFiche()
+        {
+            Console.WriteLine("\t Création d'une fiche");
+            Console.WriteLine("\t --------------------");
+
+            Array.Resize(ref DAL.repertoireActuel, DAL.repertoireActuel.Length + 1);
+            string newFiche = "";
+
+            Affichage.RemplirFicheText(ref newFiche, "Nom : ", ";");
+            Affichage.RemplirFicheText(ref newFiche, "Prenom : ", ";");
+            Affichage.RemplirFicheNum(ref newFiche, "Téléphone : ", ";");
+            Affichage.RemplirFicheNum(ref newFiche, "Code Postal : ", "");
+
+            DAL.repertoireActuel[DAL.repertoireActuel.Length - 1] = newFiche;
+            Console.WriteLine();
+            Console.WriteLine("\t**** Fiche créée ****");
+
+        }
+
+        internal static void RechercheFiche()
+        {
+            Console.WriteLine("\t Rechercher une fiche");
+            Console.WriteLine("\t ********************");
+
+            Console.Write("Valeur à rechercher : ");
+            String recherche = Console.ReadLine();
+            int index = Array.FindIndex(DAL.repertoireActuel, x => x.Contains(recherche));
+            if (index == -1)
+            {
+                Console.WriteLine();
+                Console.WriteLine("La valeur n'existe pas dans le répertoire.");
+            }
+            else Affichage.AfficherFiches(index);
+
+        }
+
+        internal static void SupprimerFiche()
+        {
+            Console.WriteLine("\t    Supprimer une fiche");
+            Console.WriteLine("\t    --------------------");
+            Affichage.AfficherFiches();
+            Console.WriteLine();
+            Console.Write("Numero de la fiche à supprimer : ");
+            string numeroStr = Console.ReadLine();
+            int numero = 0;
+            bool succes = Int32.TryParse(numeroStr, out numero);
+
+            if ((numero - 1 >= DAL.repertoireActuel.Length) || (numero - 1 < 0))
+            {
+                Console.WriteLine();
+                Console.WriteLine("\t**** Numero incorrect ****");
+                Console.WriteLine();
+                return;
+            }
+
+            for (int i = numero - 1; i < DAL.repertoireActuel.Length - 1; i++)
+                DAL.repertoireActuel[i] = DAL.repertoireActuel[i + 1];
+
+            Array.Resize(ref DAL.repertoireActuel, DAL.repertoireActuel.Length - 1);
+
+            Console.WriteLine();
+            Console.WriteLine($"\t**** Fiche numero {numero} supprimée ****");
+
+
+        }
+
+        internal static void MAJFiche()
+        {
+            Console.WriteLine("\t  Mettre a jour une fiche");
+            Console.WriteLine("\t  -----------------------");
+            Affichage.AfficherFiches();
+            Console.WriteLine();
+            Console.Write("Numero de la fiche : ");
+
+            string numeroStr = Console.ReadLine();
+            int numero = 0;
+            bool succes = Int32.TryParse(numeroStr, out numero);
+            if ((numero > DAL.repertoireActuel.Length) || (numero <= 0))
+            {
+                Console.WriteLine("\t**** Numero incorrect ****");
+                return;
+            }
+
+            string fiche = DAL.repertoireActuel[numero - 1];
+            string majFiche = "";
+            Console.WriteLine("\t Nouvelles données : ");
+
+            Affichage.RemplirFicheText(ref majFiche, "Nom : ", ";");
+            Affichage.RemplirFicheText(ref majFiche, "Prenom : ", ";");
+            Affichage.RemplirFicheNum(ref majFiche, "Téléphone : ", ";");
+            Affichage.RemplirFicheNum(ref majFiche, "Code postal : ", ";");
+
+            DAL.repertoireActuel[numero - 1] = majFiche;
+            Console.WriteLine();
+            Console.WriteLine($"\t**** Fiche numero {numero} mise à jour ****");
+
+        }
+
+    }
+}
+
+
+
+
